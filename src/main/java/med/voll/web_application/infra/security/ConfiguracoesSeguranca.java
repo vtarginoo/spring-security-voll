969326aaae77acfac32258f7dfff1ada3c6cf1c3
Victor Yunes Ribeiro Von Melentovytch <victor.melentovytch@tjrj.jus.br>
2024-12-05 15:30:30 -0300

    aula1

---
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -0,0 +1,35 @@
+package med.voll.web_application.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class ConfiguracoesSeguranca {
+
+
+    @Bean
+    public UserDetailsService dadosDeUsuariosCadastrados() {
+
+        UserDetails usuario1 = User.builder()
+                .username("joao@email.com").password("{noop}joao123").build();
+
+        UserDetails usuario2 = User.builder()
+                .username("maria@email.com").password("{noop}maria123").build();
+
+        return new InMemoryUserDetailsManager(usuario1,usuario2);
+    }
+
+
+
+
+
+
+
+
+}
