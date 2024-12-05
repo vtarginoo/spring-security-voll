2af07323c9e05024ef90c0727336f0a766ee27a5
Victor Yunes Ribeiro Von Melentovytch <victor.melentovytch@tjrj.jus.br>
2024-12-05 15:57:37 -0300

    pagina login

---
+++ src/main/java/med/voll/web_application/controller/LoginController.java
@@ -0,0 +1,16 @@
+package med.voll.web_application.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping
+    public String carregaPaginaListagem(){
+        return "autenticacao/login";
+    }
+
+
+}
